8d43e46d8386ede1ec177e24ca47fb3d312c25e3
MAURER Loïc <loic.maurer@uhha.fr>
2024-09-28 20:47:04 +0200

    fix_topic

--- website_domo/website_app/mqtt_lib_sae.py
+++ website_domo/website_app/mqtt_lib_sae.py
@@ -1,6 +1,6 @@
-# /usr/bin/python
+#!/usr/bin/python
 
-## Author Maurer Loïc
+## Author: Maurer Loïc
 ## Université Haute Alsace
 
 import paho.mqtt.client as mqtt
@@ -15,10 +15,9 @@ class MqttConnexion:
 
     USERNAME = "serveur-rpi"
 
-    def __init__(self, broker: str = "broker.Id00l.eu", port: int = 14022, topic: str = "alpha/1") -> None:
+    def __init__(self, broker: str = "broker.Id00l.eu", port: int = 14022) -> None:
         self.broker = broker
         self.port = port
-        self.topic = topic
 
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
@@ -28,12 +27,11 @@ class MqttConnexion:
     def give_feedback(self, message, feedback_topic):
         """Envoi d'un message de feedback sur un TOPIC
         
-        :param message : Message à envoyer sur le topic
-        :type message : str
+        :param message: Message à envoyer sur le topic
+        :type message: str
         
-        :param feedback_topic : Topic sur lequel envoyer le feedback
-        :type feedback_topic : str
-        
+        :param feedback_topic: Topic sur lequel envoyer le feedback
+        :type feedback_topic: str
         """
         feed = f"Envoi de feedback : {message} sur le topic {feedback_topic}"
         logging.info(feed)
@@ -44,14 +42,14 @@ class MqttConnexion:
         else:
             logging.error(f"Échec de la publication sur {feedback_topic}")
 
-    def state_led(self, message, led_topic="sae301/led"):
+    def state_led(self, message, led_topic):
         """Envoi du message d'état à la LED
         
-        :param message : Message à envoyer sur le topic
-        :type message : str
+        :param message: Message à envoyer sur le topic
+        :type message: str
         
-        :param led_topic : Topic sur lequel envoyer le message
-        :type led_topic : str : default to sae301/led
+        :param led_topic: Topic sur lequel envoyer le message
+        :type led_topic: str
         """
         result = self.client.publish(led_topic, message)
         
@@ -63,45 +61,43 @@ class MqttConnexion:
     def get_temp(self, temp_topic='sae301/temperature'):
         """Souscription au topic de température
         
-        :param temp_topic : Topic à s'abonner pour la température
-        :type temp_topic : str default to sae301/temperature
-        
+        :param temp_topic: Topic à s'abonner pour la température
+        :type temp_topic: str
         """
         self.client.subscribe(temp_topic)
         logging.info(f"{Fore.YELLOW}Souscription au topic : {temp_topic}{Style.RESET_ALL}")
 
+    def handle_light(self, action):
+        led_topic_1 = "sae301/led"
+        led_topic_2 = "sae301_2/led"
 
-    def handle_light(self,action):
         if action == "lumiere1_on":
-            self.state_led("LED_ON")
-            self.give_feedback("LED_ON")
+            self.state_led("LED_ON", led_topic_1)
+            self.give_feedback("LED_ON", "sae301/led/status")
         elif action == "lumiere1_off":
-            self.state_led("LED_OFF")
-            self.give_feedback("LED_OFF")
+            self.state_led("LED_OFF", led_topic_1)
+            self.give_feedback("LED_OFF", "sae301/led/status")
         elif action == "lumiere2_on":
-            self.state_led("LED_ON")
-            self.give_feedback("LED_ON")
+            self.state_led("LED_ON", led_topic_2)
+            self.give_feedback("LED_ON", "sae301_2/led/status")
         elif action == "lumiere2_off":
-            self.state_led("LED_OFF")
-            self.give_feedback("LED_OFF")
+            self.state_led("LED_OFF", led_topic_2)
+            self.give_feedback("LED_OFF", "sae301_2/led/status")
         elif action == "all_on":
-            self.state_led("LED_ON") and self.state_led("LED_ON")
-            self.give_feedback("LEDS ALLUMEES")
+            self.state_led("LED_ON", led_topic_1)
+            self.state_led("LED_ON", led_topic_2)
+            self.give_feedback("LEDS ALLUMEES", "sae301/led/status")
         elif action == "all_off":
-            self.state_led("LED_OFF") and self.state_led("LED_OFF")
-            self.give_feedback("LEDS ETEINTES")
-        
-##################################################################
-##################################################################
-####################Fonction de la doc Paho#######################
-
+            self.state_led("LED_OFF", led_topic_1)
+            self.state_led("LED_OFF", led_topic_2)
+            self.give_feedback("LEDS ETEINTES", "sae301/led/status")
 
     def on_connect(self, client, userdata, flags, reason_code, properties=None):
         """Callback appelé lors de la connexion au broker"""
         if reason_code == 0:
             logging.info("Connexion au broker réussie :)")
-            client.subscribe(self.topic)
-            logging.info(f"{Fore.YELLOW}Abonné au topic : {self.topic}{Style.RESET_ALL}")
+            client.subscribe("sae301/temperature")  # Subscribe to temperature topic
+            logging.info(f"{Fore.YELLOW}Abonné au topic : sae301/temperature{Style.RESET_ALL}")
         else:
             logging.error(f"Erreur de connexion : {reason_code}")
 
@@ -113,7 +109,6 @@ class MqttConnexion:
     def on_disconnect(self, client, userdata, rc):
         """Callback appelé lors de la déconnexion"""
         logging.info(f"Déconnexion avec le code de retour {rc}")
-
 
     def handle_connexion(self):
         """Gère la connexion au broker MQTT et écoute les messages"""
--- website_domo/website_app/views.py
+++ website_domo/website_app/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 from .mqtt_lib_sae import MqttConnexion
 
 def home(request):
-    connexion = MqttConnexion(topic="sae301/led/status")
-    
+    connexion = MqttConnexion()
+
     if request.method == "POST":
         action = request.POST.get('action')
         if action:
             connexion.handle_light(action)
-    
-    temp = connexion.get_temp()
-    
+
+    temp_topic = 'sae301/temperature'
+    connexion.get_temp(temp_topic)
+    temp = connexion.client.loop()
+
     return render(request, "index.html", {"temp": temp})
