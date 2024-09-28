4674497e02f410a391c02e54e91cf13df6d23384
MAURER Loïc <loic.maurer@uhha.fr>
2024-09-28 16:45:06 +0200

    push_import_view

--- website_domo/website_app/views.py
+++ website_domo/website_app/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import FileResponse
+from reportlab.lib.pagesizes import letter
+from .models import *
+import csv
+from mqtt_lib_sae import MqttConnexion
 
 def home(request):
     return HttpResponse("Bienvenue sur la page d'accueil de votre site !")
